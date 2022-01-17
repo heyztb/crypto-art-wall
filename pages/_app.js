c712c319252f0b31015ca4452068971fdb60e507
Zach Blake <ztb@pm.me>
2022-01-16 20:42:25 -0800

    setup @solana/wallet-adapters-react

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,46 @@
+import React, { useMemo } from 'react';
+import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react';
+import { WalletAdapterNetwork } from '@solana/wallet-adapter-base';
+import { clusterApiUrl } from '@solana/web3.js';
+import {
+  LedgerWalletAdapter,
+  PhantomWalletAdapter,
+  SlopeWalletAdapter,
+  SolflareWalletAdapter,
+  SolletWalletAdapter,
+  SolletExtensionWalletAdapter,
+  TorusWalletAdapter
+} from '@solana/wallet-adapter-wallets';
+import {
+  WalletModalProvider,
+} from '@solana/wallet-adapter-react-ui';
+
 import '../styles/globals.css'
+require("@solana/wallet-adapter-react-ui/styles.css");
 
 function ArtWall({ Component, pageProps }) {
-  return <Component {...pageProps} />
+
+  const network = WalletAdapterNetwork.Devnet;
+  const endpoint = useMemo(() => clusterApiUrl(network), [network])
+  const wallets = useMemo(() => [
+    new PhantomWalletAdapter(),
+    new SlopeWalletAdapter(),
+    new SolflareWalletAdapter(),
+    new TorusWalletAdapter(),
+    new LedgerWalletAdapter(),
+    new SolletWalletAdapter({network}),
+    new SolletExtensionWalletAdapter({network}),
+  ], [network])
+
+  return (
+    <ConnectionProvider endpoint={endpoint}> 
+      <WalletProvider wallets={wallets} autoConnect>
+        <WalletModalProvider>
+          <Component {...pageProps } />
+        </WalletModalProvider>
+      </WalletProvider>
+    </ConnectionProvider>
+  )
 }
 
 export default ArtWall 
