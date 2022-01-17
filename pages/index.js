fd3d5b69e4b3dcc961ebda635ad81d4f8f695e12
Zach Blake <ztb@pm.me>
2022-01-16 21:45:56 -0800

    reduce vertical spacing and add tagline

--- pages/index.js
+++ pages/index.js
@@ -11,9 +11,9 @@ export default function Home() {
 
       <main id="main">
         <div className='bg-slate-800 h-[100vh] w-full'>
-          <MenuBar />
-          <div className='pt-10 mx-auto w-96 grid grid-cols-1 space-y-6'>
+          <div className='pt-10 mx-auto w-96 grid grid-cols-1 space-y-1'>
             <h1 className="font-bold subpixel-antialiased h-16 text-5xl text-center text-transparent bg-clip-text bg-gradient-to-r from-indigo-700 via-indigo-400 to-cyan-500">Crypto Art Wall</h1>
+            <p className="text-xl text-slate-50 text-center">A public art collection powered by Solana</p>
           </div>
         </div>
       </main>
