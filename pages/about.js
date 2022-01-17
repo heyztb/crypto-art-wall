ff72f4bcaa565b80d6d29655e81a4afa53dcc07d
Zach Blake <ztb@pm.me>
2022-01-16 23:41:44 -0800

    setup page to keep with theme of the site

--- pages/about.js
+++ pages/about.js
@@ -1,5 +1,19 @@
+import Head from "next/head"
+
 export default function About() {
   return (
-    <p>TODO</p>
+    <div>
+      <Head>
+        <title>About the art wall</title>
+        <meta name="description" content="a public art collection powered by solana" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main id="main">
+        <div className='bg-slate-800 h-[100vh] w-full'>
+        {/* TODO */}
+        </div>
+      </main>
+    </div>
   )
 }
