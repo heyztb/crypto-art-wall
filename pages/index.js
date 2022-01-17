8f47197a96f14a4cd74070c323f587fc78d7294f
Zach Blake <ztb@pm.me>
2022-01-16 20:42:00 -0800

    add menubar component to page

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import MenuBar from '../components/MenuBar'
 export default function Home() {
   return (
     <div>
@@ -8,8 +9,13 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main>
-        <div className=""></div>
+      <main id="main">
+        <div className='bg-slate-800 h-[100vh] w-full'>
+          <MenuBar />
+          <div className='pt-10 mx-auto w-96 grid grid-cols-1 space-y-6'>
+            <h1 className="font-bold subpixel-antialiased h-16 text-5xl text-center text-transparent bg-clip-text bg-gradient-to-r from-indigo-700 via-indigo-400 to-cyan-500">Crypto Art Wall</h1>  
+          </div>
+        </div>
       </main>
 
       <footer>
