058316985b149b55afda50f4685525ef71e08cae
Zach Blake <ztb@pm.me>
2022-01-17 00:30:05 -0800

    update padding and add todo

--- pages/index.js
+++ pages/index.js
@@ -10,12 +10,12 @@ export default function Home() {
 
       <main id="main">
         <div className='bg-slate-800 h-[100vh] w-full'>
-          <div className='pt-10 mx-auto w-96 grid grid-cols-1 space-y-1'>
+          <div className='py-10 mx-auto w-96 grid grid-cols-1'>
             <h1 className="font-bold subpixel-antialiased h-16 text-5xl text-center text-transparent bg-clip-text bg-gradient-to-r from-indigo-700 via-indigo-400 to-cyan-500">Crypto Art Wall</h1>
             <p className="text-xl sm:text-lg text-slate-50 text-center">A public art collection on the Solana blockchain</p>
           </div>
-          <div className="py-10 px-10 grid grid-cols-3 space-x-4 space-y-4">
-
+          <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+          {/* TODO: Write logic to retrieve art and display it here */}
           </div>
         </div>
       </main>
