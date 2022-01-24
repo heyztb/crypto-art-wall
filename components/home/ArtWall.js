988f25109952122c7a80a97fc3fd0d74d42195eb
Zach Blake <ztb@pm.me>
2022-01-23 16:16:17 -0800

    wip: artwall componenent should be able to fetch art now

--- components/home/ArtWall.js
+++ components/home/ArtWall.js
@@ -1,7 +1,26 @@
+import { useState, useEffect, useCallback } from 'react'
+import { useSolana } from '../../hooks/useSolana'
 export default function ArtWall() {
+
+  const { program, artWallPubKey } = useSolana();
+  const [ collection, setCollection ] = useState([])
+
+  const getCollection = useCallback(async () => {
+    const account = await program.account.artWallAccount.fetch(artWallPubKey)
+    setCollection(account.artCollection);
+  }, [artWallPubKey, program.account.artWallAccount])
+
+  useEffect(() => {
+    getCollection()
+  }, [collection, getCollection])
+
   return (
     <div>
-      {/* TODO */}
+      {collection.map((art, index) => {
+        return (
+          <li key={index}>{art.submitted_by}</li>
+        )
+      })}
     </div>
   )
 }
