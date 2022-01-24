1d2d9e033d258232e49e1d8bf1c8944c5760c09d
Zach Blake <ztb@pm.me>
2022-01-23 16:18:35 -0800

    add useSolana hook

---
+++ hooks/useSolana.js
@@ -0,0 +1,28 @@
+import { Connection, PublicKey, clusterApiUrl } from "@solana/web3.js";
+import { Provider, Program } from "@project-serum/anchor";
+import { WalletAdapterNetwork } from "@solana/wallet-adapter-base";
+import { useWallet } from "@solana/wallet-adapter-react";
+
+import idl from '../idl.json';
+
+const opts = {
+  preflightCommitment: "processed"
+};
+
+const network = clusterApiUrl(WalletAdapterNetwork.Devnet);
+const connection = new Connection(network, opts.preflightCommitment);
+
+export function useSolana() {
+  const { wallet } = useWallet();
+
+  const provider = new Provider(connection, wallet, opts.preflightCommitment);
+
+  const programId = new PublicKey(idl.metadata.address);
+  const program = new Program(idl, programId, provider);
+
+  const arr = Object.values(JSON.parse(process.env.NEXT_PUBLIC_ARTWALL_PUBLIC_KEY));
+  const publicKey = new Uint8Array(arr);
+  const artWallPubKey = new PublicKey(publicKey).toBase58();
+
+  return { program, artWallPubKey };
+}
