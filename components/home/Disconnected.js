ccd458e3438b0201065bcdfabe427ab039c21a48
Zach Blake <ztb@pm.me>
2022-01-23 16:16:48 -0800

    wip: disconnected component is very basic for now

--- components/home/Disconnected.js
+++ components/home/Disconnected.js
@@ -1,7 +1,10 @@
 export default function Disconnected() {
   return (
-    <div>
-      {/* TODO */}
+    <div className="flex flex-col justify-center space-y-2">
+      <h1 className="text-2xl text-center text-slate-50">
+        Your wallet is not connected
+      </h1>
+      <p className="text-xl text-center text-slate-50">Please connect your Solana wallet in order to submit art to the collection</p>
     </div>
   )
 }
