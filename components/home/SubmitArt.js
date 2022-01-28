a0d09dabdbf57f3bb8e5d0f9009f74dc2e85a5d6
Zach Blake <ztb@pm.me>
2022-01-27 20:13:54 -0800

    working out art submission logic

--- components/home/SubmitArt.js
+++ components/home/SubmitArt.js
@@ -1,32 +1,72 @@
+import { useWallet } from "@solana/wallet-adapter-react";
 import { useCallback } from "react"
 import { useDropzone } from "react-dropzone"
+import { useSolana } from '../../hooks/useSolana';
 
 export default function SubmitArt() {
 
+  const { artWallPubKey, program } = useSolana();
+  const { publicKey } = useWallet();
   const onDrop = useCallback(async (acceptedFiles) => {
-    console.log(acceptedFiles)
+    await handleUpload(acceptedFiles);
+  }, [handleUpload])
+
+  const handleUpload = useCallback(async (files) => {
+    const form = new FormData();
+    files.map((file) => {
+      form.append(file.name, file)
+    })
+
+    try {
+      const res = await fetch('/api/upload', {
+        method: 'POST',
+        body: form
+      })
+      
+      const { hash } = await res.json();
+      await uploadToSolana(hash);
+    } catch (e) {
+      console.log(e);
+    }
+  }, [uploadToSolana])
+
+  const uploadToSolana = useCallback(async (hash) => {
+    try {
+      let tx = await program.rpc.submitArt(hash, {
+        accounts: {
+          artwallAccount: artWallPubKey,
+          user: publicKey,
+        }
+      });
+
+      console.log(tx);
+    } catch (e) {
+      console.log(e);
+    }
   }, [])
 
   const { getRootProps, getInputProps, isDragActive, isDragReject } = useDropzone({
     onDrop,
-    accept: 'image/*'
+    accept: 'image/*',
+    multiple: false,
   })
 
   return (
     <div className="flex justify-center">
       <div className="flex justify-center bg-slate-700 opacity-75 rounded-md w-2/5 h-24" {...getRootProps()}>
-        <label htmlFor="upload-art" className="w-full h-full flex flex-col">
-          <div className="flex flex-col items-center justify-center pt-5 space-y-2">
-            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 text-gray-400" viewBox="0 0 20 20" fill="currentColor">
-              <path fillRule="evenodd" d="M4 3a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V5a2 2 0 00-2-2H4zm12 12H4l4-8 3 6 2-4 3 6z" clipRule="evenodd" />
-            </svg>
-            <p className={`${isDragReject ? 'text-red-400' : 'text-gray-400'} pt-1 text-sm tracking-wider`}>
-              { isDragActive && !isDragReject ? "Drop to upload art" : "Drag and drop or click to upload art" }
-              { isDragActive && isDragReject && 'Only images are allowed at this time' }
-            </p>
-          </div>
-          <input type="file" className="opacity-0" {...getInputProps()} />
-        </label>
+        <form action="/api/upload" method="post">
+          <label htmlFor="upload-art" className="w-full h-full flex flex-col">
+            <div className="flex flex-col items-center justify-center pt-5 space-y-2">
+              <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 text-gray-400" viewBox="0 0 20 20" fill="currentColor">
+                <path fillRule="evenodd" d="M4 3a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V5a2 2 0 00-2-2H4zm12 12H4l4-8 3 6 2-4 3 6z" clipRule="evenodd" />
+              </svg>
+              <p className='text-gray-400 pt-1 text-sm tracking-wider'>
+                { isDragActive && !isDragReject ? "Drop to upload art" : "Drag and drop or click to upload art" }
+              </p>
+            </div>
+            <input type="file" name="upload-art" {...getInputProps()} />
+          </label>
+      </form>
       </div>
     </div>
   )
