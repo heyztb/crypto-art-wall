283626ee89685f634b809091eec64586621af964
Zach Blake <ztb@pm.me>
2022-01-28 18:21:36 -0800

    unused variable

--- pages/api/upload.js
+++ pages/api/upload.js
@@ -7,8 +7,6 @@ export const config = {
   }
 }
 
-// returning a promise from this function because for some reason
-// i was receiving a warning about a resolved request with no response
 const handler = async (req, res) => {
   const promise = new Promise((resolve, reject) => {
     const pinata = pinataSDK(process.env.PINATA_API_KEY, process.env.PINATA_API_SECRET);
@@ -17,7 +15,7 @@ const handler = async (req, res) => {
       keepExtensions: true,
     });
     
-    form.parse(req, (err, fields, files) => {
+    form.parse(req, (err, _fields, files) => {
       if (err) {
         console.log(err);
         reject(err);
