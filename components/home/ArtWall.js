2f1d6d55a3caa03cea79b3ec8cbf50ddd530d76c
Zach Blake <ztb@pm.me>
2022-01-28 18:19:51 -0800

    redux made this nice and simple

--- components/home/ArtWall.js
+++ components/home/ArtWall.js
@@ -1,39 +1,33 @@
-import { useState, useEffect, useCallback } from 'react'
-import { useSolana } from '../../hooks/useSolana'
-import SubmitArt from './SubmitArt';
+import { useGetCollectionQuery } from '../../services/collection';
+import SubmitArt from './SubmitArt'
 export default function ArtWall() {
-
-  const { program, artWallPubKey } = useSolana();
-  const [ collection, setCollection ] = useState([])
-  const [ error, setError ] = useState(null)
-
-  const getCollection = useCallback(async () => {
-    try {
-      const account = await program.account.artWallAccount.fetch(artWallPubKey)
-      setCollection(account.artCollection);
-    } catch (e) {
-      setError("Unable to fetch art collection, refresh and try again.")
-    }
-  }, [artWallPubKey])
-
-  useEffect(() => {
-    getCollection()
-  }, [getCollection])
+  const { data, error, isLoading  } = useGetCollectionQuery('', {
+    pollingInterval: 5000
+  });
 
   return (
     <div className='flex flex-col justify-center'>
-      {!error && collection.length > 0 && collection.map((art, index) => {
-        return (
-          <li key={index}>{art.submittedBy.toString()}</li>
-        )
-      })}
-      {!error && collection.length === 0 && 
-        <div className="flex-justify-center py-6">
+      {isLoading &&
+        <div className="flex justify-center">
+          <p className="text-lg text-white text-center">Loading collection...</p>
+        </div>
+      }
+      {data && data.collection.length === 0 &&
+        <div className="flex justify-center">
           <p className="text-lg text-emerald-400 text-center">No art has been submitted yet! Will you be the first?</p>
         </div>
       }
+      {data && data.collection.length > 0 && 
+        <div className="grid grid-cols-2">
+          {data.collection.map((art, index) => (
+            <li key={index}>
+              {art.submittedBy.toString()}
+            </li>
+          ))}
+        </div>
+      }
       {error && 
-        <div className='flex justify-center py-6'>
+        <div className='flex justify-center'>
           <p className='text-lg text-red-400 text-center'>{error}</p>
         </div>
       }
